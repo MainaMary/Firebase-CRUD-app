87fb7c6f30528b164dee1885ad796c0123071a4c
Mary <marieshix98@gmail.com>
2023-02-27 23:38:21 +0300

    Add useNavigate hook to navigate programmatically

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import CustomButton from './CustomButton'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate()
   return (
-    <nav className='h-24 flex justify-between items-center bg-slate-500'>
-      <h3>Gallery Logo</h3>
-      <h3>User profile</h3>
+    <nav className='h-24 flex justify-between items-center'>
+      <h3 onClick={() =>navigate('/')}>Gallery Logo</h3>
+      <div className='flex'>
+        <CustomButton onClick={() =>navigate('/login')}>Sign in</CustomButton>
+        <CustomButton onClick={() => navigate('/register')}>Sign up</CustomButton>
+      </div>
+     
     </nav>
   )
 }
