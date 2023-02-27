393ee2521db9575058e46ad0a22cadcf1f09daca
Mary <marieshix98@gmail.com>
2023-02-27 23:24:24 +0300

    Add suspense component

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import AuthContextProvider from './context/authContext'
+import CustomLoader from './components/CustomLoader'
+import { Suspense } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AuthContextProvider>
+    <Suspense fallback={<CustomLoader/>}>
     <App />
+    </Suspense>
+  
   </AuthContextProvider>,
 )
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,21 +1,26 @@
+import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
-import Homepage from "../pages/Homepage";
+
 import RootLayout from "../layout/RootLayout";
-import Signin from "../pages/auth/Signin";
-import Signup from "../pages/auth/Signup";
+const Homepage = React.lazy(() => import('../pages/Homepage'))
+const Signin  = React.lazy(() => import('../pages/auth/Signin'))
+const Signup = React.lazy(() =>import('../pages/auth/Signup'))
+const ImageList = React.lazy(() =>import('../pages/list/ImageList'))
 const ROOT = "/";
 const SIGNIN = "/login";
 const REGISTER = "/register";
+const LIST = "/imageList"
 export const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path={ROOT} element={<RootLayout />}>
       <Route index element={<Homepage />} />
       <Route path={SIGNIN} element={<Signin />} />
       <Route path={REGISTER} element={<Signup />} />
+      <Route path={LIST} element={<ImageList/>}/>
       <Route />
     </Route>
   )
