92b0e37d7922cb200ef5e45a8538a6cbb7a72426
Mary <marieshix98@gmail.com>
2023-02-28 23:44:14 +0300

    Add image button on the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import CustomButton from './CustomButton'
 import { useNavigate } from 'react-router-dom'
+import Modal from './Modal'
+import { useAuthContext } from '../context/authContext'
 
 const Navbar = () => {
   const navigate = useNavigate()
+  const {edit,openModal, handleModal} = useAuthContext()
   return (
-    <nav className='h-24 flex justify-between items-center'>
+    <nav className='h-24 flex   w-full justify-between items-center'>
       <h3 onClick={() =>navigate('/')}>Gallery Logo</h3>
       <div className='flex'>
         <CustomButton onClick={() =>navigate('/login')}>Sign in</CustomButton>
         <CustomButton onClick={() => navigate('/register')}>Sign up</CustomButton>
       </div>
-     
+      <div>
+      <CustomButton onClick={handleModal}>Add image</CustomButton>
+      </div>
+        {openModal  && <Modal openModal={openModal} handleModal={handleModal}/>}
     </nav>
   )
 }
