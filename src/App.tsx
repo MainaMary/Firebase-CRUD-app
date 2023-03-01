4924e1759a4e25ddee97a9f2726a519da930213f
Mary <marieshix98@gmail.com>
2023-03-01 16:11:45 +0300

    Add responsive breakpoint and image banner

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className='w-full md:max-w-[1240px] m-auto'>
+    <div className='w-full md:max-w-[1240px]  px-12 md:px-0 m-auto'>
       <RouterProvider router={routes} fallbackElement={<CustomLoader />}/>
     </div>
   )
