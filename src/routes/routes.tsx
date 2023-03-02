243b39941654c4eba0e843e2aef7c60988131894
Mary <marieshix98@gmail.com>
2023-03-02 23:55:23 +0300

    Update routes component with NotFound component

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,10 +10,12 @@ const Homepage = React.lazy(() => import('../pages/Homepage'))
 const Signin  = React.lazy(() => import('../pages/auth/Signin'))
 const Signup = React.lazy(() =>import('../pages/auth/Signup'))
 const ImageList = React.lazy(() =>import('../pages/list/ImageList'))
+const NotFound = React.lazy(() =>import('../pages/NotFound'))
 const ROOT = "/";
 const SIGNIN = "/login";
 const REGISTER = "/register";
 const LIST = "/imageList"
+const NOTFOUND = "*"
 export const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path={ROOT} element={<RootLayout />}>
@@ -21,6 +23,7 @@ export const routes = createBrowserRouter(
       <Route path={SIGNIN} element={<Signin />} />
       <Route path={REGISTER} element={<Signup />} />
       <Route path={LIST} element={<ImageList/>}/>
+      <Route path={NOTFOUND} element={<NotFound/>}/>
       <Route />
     </Route>
   )
