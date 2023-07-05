63a7d2b258eab36b1271f75a46e531a5793fc07e
Mary <marieshix98@gmail.com>
2023-07-05 23:52:27 +0300

    import taost conatiner component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { routes } from './routes/routes'
 import { RouterProvider } from 'react-router-dom'
 import CustomLoader from './components/CustomLoader'
 import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className='w-full md:max-w-[1240px]  px-12 md:px-0 m-auto'>
+      <ToastContainer/>
       <RouterProvider router={routes} fallbackElement={<CustomLoader />}/>
     </div>
   )
