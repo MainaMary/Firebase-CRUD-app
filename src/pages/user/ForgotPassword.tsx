4a457d245ba9a422d5144594245a22c551c4330c
Mary <marieshix98@gmail.com>
2023-07-15 18:24:11 +0300

    fix display of change password modal

--- src/pages/user/ForgotPassword.tsx
+++ src/pages/user/ForgotPassword.tsx
@@ -15,14 +15,21 @@ const ForgotPassword = ({pswdModal, handleModal}:ModalProps) => {
   if(!pswdModal) return null
   return (
     <div className='fixed  flex justify-center w-ful h-full items-center bg-[#171616] opacity-95 top-0 left-0 right-0 z-50  p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full'>
-     <div className='rounded-md bg-white w-full md:w-[40%] px-8 py-3 flex h-auto items-center justify-between'>
-      <p></p>
-      <p onClick={handleModal} className=' cursor-pointer text-3xl font-semibold'>X</p>
-      <form>
+     <div className='rounded-md bg-white w-full md:w-[30%] px-8 py-3 flex h-auto items-center justify-between'>
+      <div className='w-full' >
+        <div className='flex justify-between'>
+        <p>Change password</p>
+      <p onClick={handleModal} className=' cursor-pointer text-2xl font-semibold'>X</p>
+        </div>
+       
+      <form className='w-ful'>
         <CustomLabel>Email addres</CustomLabel>
         <CustomInput type="text" name="email" onChange={handleChange} value={email}/>
-        <CustomButton>Change password</CustomButton>
+        <CustomButton className='my-4'>Change password</CustomButton>
       </form>
+      </div>
+
+     
      </div>
     </div>
   )
